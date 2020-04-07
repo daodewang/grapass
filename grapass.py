8bffc914855d28f159df83fd386516e9b45c711e
toad@mac <daodewang@163.com>
2020-04-08 07:35:59 +0800

    start

--- grapass.py
+++ grapass.py
@@ -1,6 +1,31 @@
 # analysis the grapass of cellphone
 
 import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
+
+N = 2001
+M = 8000
+
+G = nx.generators.random_graphs.gnm_random_graph(N,M)
+while(nx.algorithms.connectivity.edge_augmentation.is_k_edge_connected(G,1) == False):
+    print('gen G')
+    G = nx.generators.random_graphs.gnm_random_graph(N, M)
+nx.draw(G)
+plt.show()
+
+betweeness = nx.algorithms.centrality.betweenness_centrality(G)
+print(betweeness)
+T = 0
+for v in betweeness.values():
+    T += v
+
+avlen = nx.algorithms.shortest_paths.generic.average_shortest_path_length(G)
+print(avlen)
+print(T)
+
+
+
 
 """
 a b c
@@ -10,7 +35,7 @@ g h i
 
 """
 a c i g b d f h e
-"""
+
 
 
 neib = [[0,0,0,0,1,1,0,0,1],
@@ -28,3 +53,4 @@ m = np.array(neib)
 print(m)
 
 print(m*m)
+"""
